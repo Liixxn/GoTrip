0b36fcbe46f6bfdb27b59b9ad2064643c43408f7
Lian Salmerón López <lembajosa@gmail.com>
2023-05-09 16:45:41 +0200

    SplashScreen

--- GoTrip/src/app/app-routing.module.ts
+++ GoTrip/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  { path: 'splash-screen', component: SplashScreenComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
