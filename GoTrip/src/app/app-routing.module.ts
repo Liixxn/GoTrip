ca0fb935a5d101a18bb617e6d8083664a762cc8b
Lian Salmerón López <lembajosa@gmail.com>
2023-05-10 00:19:27 +0200

    Comienzo del home y barra de navegacion

--- GoTrip/src/app/app-routing.module.ts
+++ GoTrip/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
-  { path: 'splash-screen', component: SplashScreenComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent }
+
 ];
 
 @NgModule({
--- GoTrip/src/app/app.module.ts
+++ GoTrip/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +21,12 @@ import { HomeComponent } from './views/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
