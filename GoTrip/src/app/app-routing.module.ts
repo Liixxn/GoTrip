3e80993320fd2e54c873096d0c447d940e25f000
Rubén Martínez Gormaz <rubenmartinezgormaz@gmail.com>
2023-05-12 12:49:54 +0200

    Ventana TourList
    Falta el responsive y dos iconos

--- GoTrip/src/app/app-routing.module.ts
+++ GoTrip/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
+import { TourListComponent } from './views/tour-list/tour-list.component';
 
 const routes: Routes = [
   { path: 'splash-screen', component: SplashScreenComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'tour-list', component: TourListComponent },
 ];
 
 @NgModule({
