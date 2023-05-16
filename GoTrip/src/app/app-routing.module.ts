b07f1497c362d6502701a69d3da74f0a30f32be6
Camiwi <camilobetancur@outlook.com>
2023-05-16 18:43:52 +0200

    primera implentacion del perfil
    editado angular.json para implementar boot, foto de perfil y background practicamente tal cual lo tenemos en figma, queda el carousel personalizado del user

--- GoTrip/src/app/app-routing.module.ts
+++ GoTrip/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
+import { PerfilComponent } from './views/perfil/perfil.component';
 
 const routes: Routes = [
   { path: 'splash-screen', component: SplashScreenComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'perfil', component: PerfilComponent},
 ];
 
 @NgModule({
--- GoTrip/src/app/app.module.ts
+++ GoTrip/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
+import { PerfilComponent } from './views/perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
-    HomeComponent
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
