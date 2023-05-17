17cdd639bfc033f911c0c2281c9d6ee308935939
Rubén Martínez Gormaz <rubenmartinezgormaz@gmail.com>
2023-05-17 19:13:56 +0200

    configuracion

--- GoTrip/src/app/app-routing.module.ts
+++ GoTrip/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
+import { ConfiguracionComponent } from './views/configuracion/configuracion.component';
 
 const routes: Routes = [
   { path: 'splash-screen', component: SplashScreenComponent },
   { path: 'home', component: HomeComponent },
+  {path: 'configuracion', component: ConfiguracionComponent}
 ];
 
 @NgModule({
--- GoTrip/src/app/app.module.ts
+++ GoTrip/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashScreenComponent } from './views/splash-screen/splash-screen.component';
 import { HomeComponent } from './views/home/home.component';
+import { ConfiguracionComponent } from './views/configuracion/configuracion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
-    HomeComponent
+    HomeComponent,
+    ConfiguracionComponent
   ],
   imports: [
     BrowserModule,
