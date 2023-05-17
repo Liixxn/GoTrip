2462403b6eb61bd39010068849696a69e447e55b
Lian Salmerón López <lembajosa@gmail.com>
2023-05-17 21:46:53 +0200

    Carrusel de imágenes horizontales

--- GoTrip/src/app/app-routing.module.ts
+++ GoTrip/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegistroComponent } from './views/registro/registro.component';
 import { TourListComponent } from './views/tour-list/tour-list.component';
-import { GuardadosComponent} from "./views/guardados/guardados.component";
+import { GuardadosComponent } from './views/guardados/guardados.component';
 import { PerfilComponent } from './views/perfil/perfil.component';
-import { CarruselComponent} from "./views/carrusel/carrusel.component";
+import { CarruselComponent } from "./views/carrusel/carrusel.component";
 import { ConfiguracionComponent } from './views/configuracion/configuracion.component';
 
 const routes: Routes = [
