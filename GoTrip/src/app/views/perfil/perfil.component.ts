3304ca6778f86beb23b9d610285076619a97d295
Camiwi <camilobetancur@outlook.com>
2023-05-19 12:17:16 +0200

    perfil casi completo
    nuevas imagenes
    apartado para el nombre
    vertical scroll en todo el componente
    los tabs se destacan con un circulo al seleccionar
    nuevas fuentes igual que figma

--- GoTrip/src/app/views/perfil/perfil.component.ts
+++ GoTrip/src/app/views/perfil/perfil.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
+  activeWrapperIndex = 1; // Set the default active wrapper index
 
-}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  scrollToWrapper(index: number) {
+    const targetWrapper = document.querySelector(`#scrollWrapper${index}`);
+    if (targetWrapper) {
+      targetWrapper.scrollIntoView({ behavior: 'smooth' });
+
+      const scrollWrappers = document.querySelectorAll('.scrolling-wrapper');
+      scrollWrappers.forEach((wrapper, wrapperIndex) => {
+        if (wrapperIndex + 1 !== index) {
+          wrapper.classList.add('hidden');
+        } else {
+          wrapper.classList.remove('hidden');
+        }
+      });
+
+      this.activeWrapperIndex = index; // Update the active wrapper index
+    }
+    return false; // Prevent the default behavior of the tab link
+  }}
