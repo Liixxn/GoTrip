e34ae97879a67691ea0748ac6c30ff406ebde8a0
Rubén Martínez Gormaz <rubenmartinezgormaz@gmail.com>
2023-05-31 17:23:36 +0200

    Corrección de errores

--- GoTrip/src/app/views/tour-list/tour-list.component.ts
+++ GoTrip/src/app/views/tour-list/tour-list.component.ts
@@ -26,31 +26,42 @@ export class TourListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.activatedRoute.paramMap.pipe(
-      switchMap(params => {
-        this.pais = params.get('pais');
-
-        switch (this.pais) {
-          case 'Japón':
-            this.imagenPais = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/16/a6/88/con-la-primavera-in-giappone.jpg?w=700&h=-1&s=1";
-            this.media = 4.54;
-            return this.http.get<any>('/assets/files/japon.txt');
-          case 'USA':
-            this.imagenPais = "https://eldiariony.com/wp-content/uploads/sites/2/2021/04/nueva-york-shutterstock_57571180.jpg?quality=75&strip=all&w=1200";
-            this.media = 4.45;
-            return this.http.get<any>('/assets/files/usa.txt');
-          case 'España':
-            this.imagenPais =  "https://images.ecestaticos.com/6lNdBlA5P-RyU-ZAW6a-1rkUeJY=/1x118:2271x1394/1600x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fdca%2Fe5e%2Fc23%2Fdcae5ec23094c61634c4abf5b395b05c.jpg";
-            this.media = 4.33;
-            return this.http.get<any>('/assets/files/spain.txt');
-          default:
-            return [];
-        }
-      })
-    ).subscribe(data => {
-      this.ciudades = data[this.pais || ''] || [];
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.pais = params.get('pais');
+      this.loadCountryData(this.pais);
     });
   }
+
+  loadCountryData(pais: string | null) {
+    switch (pais) {
+      case 'Japón':
+        this.imagenPais = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/16/a6/88/con-la-primavera-in-giappone.jpg?w=700&h=-1&s=1";
+        this.media = 4.54;
+        this.http.get<any>('/assets/files/japon.txt').subscribe(data => {
+          this.ciudades = data[pais || ''] || [];
+        });
+        break;
+      case 'USA':
+        this.imagenPais = "https://eldiariony.com/wp-content/uploads/sites/2/2021/04/nueva-york-shutterstock_57571180.jpg?quality=75&strip=all&w=1200";
+        this.media = 4.45;
+        this.http.get<any>('/assets/files/usa.txt').subscribe(data => {
+          this.ciudades = data[pais || ''] || [];
+        });
+        break;
+      case 'España':
+        this.imagenPais =  "https://images.ecestaticos.com/6lNdBlA5P-RyU-ZAW6a-1rkUeJY=/1x118:2271x1394/1600x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fdca%2Fe5e%2Fc23%2Fdcae5ec23094c61634c4abf5b395b05c.jpg";
+        this.media = 4.33;
+        this.http.get<any>('/assets/files/spain.txt').subscribe(data => {
+          this.ciudades = data[pais || ''] || [];
+        });
+        break;
+      default:
+        this.ciudades = [];
+        break;
+    }
+  }
+
+
 
   public cargarDescripcion() {
     this.sub = this.activatedRoute.params.subscribe(params => {
